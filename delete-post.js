ba919eeb2706883d9eb2722d4b77f86da7946b7e
Tina <tina.bekkholt@hotmail.com>
2023-10-03 16:43:01 +0200

    remove fetch

--- delete-post.js
+++ delete-post.js
@@ -2,38 +2,13 @@ const apiUrl = "https://api.noroff.dev";
 const postsUrl = "/api/v1/social/posts/";
 const allPostsUrl = apiUrl + postsUrl;
 
-const remove = document.querySelector(".delete");
-remove.addEventListener("click", onClick);
+const deleteBtn = document.querySelector(".delete");
+deleteBtn.addEventListener("click", onClick);
 
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 
 const id = params.get("id");
-
-/**
- * This fetches the specific posts
- * from the API based on their IDs
- * @param {string} id
- * @returns the specific posts from ID
- */
-async function fetchSpecificPost(id) {
-  try {
-    const token = localStorage.getItem("accessToken");
-    const getData = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    };
-    const response = await fetch(allPostsUrl + `${id}`, getData);
-    const result = await response.json();
-
-    return result;
-  } catch (error) {}
-}
-
-await fetchSpecificPost(id);
 
 async function deletePost(url) {
   try {
